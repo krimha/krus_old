58d1191e0b9adfe2c223ea41d76d2d6899650798
Kristian Monsen Haug <kristian.haug95@gmail.com>
2021-05-14 11:31:25 +0200

    Instanciate Finite state machine object

--- include/parselib/finite_state_machine.h
+++ include/parselib/finite_state_machine.h
@@ -2,9 +2,11 @@
 #include <unordered_set>
 #include <map>
 
-template<typename State, typename Character>
+template<typename State_, typename Character_>
 class FiniteStateMachine {
 public:
+    using State = State_;
+    using Character = Character_;
     using StateSet = std::unordered_set<State>;
     using Alphabet = std::unordered_set<Character>;
     // TODO: Would like to use unordered_map here, but will need to define a hash function for std::pair
@@ -14,6 +16,28 @@ public:
 	    Alphabet alphabet,
 	    TransitionFunction f,
 	    State start_state,
-	    StateSet accept_states) {};
+	    StateSet accept_states
+	    ) 
+	: states_{states} 
+	, alphabet_{alphabet} 
+	, f_{f} 
+	, start_state_{start_state} 
+	, accept_states_{accept_states} 
+    {};
+
+    StateSet getStates() { return states_; }
+    Alphabet getAlphabet() { return alphabet_; };
+    TransitionFunction getTransFunc() { return f_; };
+    State getStartState() { return start_state_; };
+    StateSet getAcceptStates() {return accept_states_; };
+
+
+private:
+    StateSet states_;
+    Alphabet alphabet_;
+    TransitionFunction f_;
+    State start_state_;
+    StateSet accept_states_;
+
 
 };
--- tests/compiled_tests.cpp
+++ tests/compiled_tests.cpp
@@ -4,17 +4,24 @@
 
 TEST_CASE( "Create finite state machine" )
 {
-    FiniteStateMachine<std::string, char> machine{ 
-	{"q1", "q2", "q3"},
-	{ '0', '1' },
+    using DFA = FiniteStateMachine<std::string, char>;
+    DFA::StateSet states = {"q1", "q2", "q3"};
+    DFA::Alphabet alpha  = { '0', '1' };
+    DFA::TransitionFunction func =  
+	{{{"q1", '0'}, "q1"}
+	,{{"q1", '1'}, "q2"}
+	,{{"q2", '0'}, "q3"}
+	,{{"q2", '1'}, "q2"}
+	,{{"q3", '0'}, "q2"}
+	,{{"q3", '1'}, "q2"}};
+    DFA::State start_state = "q1";
+    DFA::StateSet accept_states = {"q2"};
 
-         {{{"q1", '0'}, "q1"}
-         ,{{"q1", '1'}, "q2"}
-         ,{{"q2", '0'}, "q3"}
-         ,{{"q2", '1'}, "q2"}
-         ,{{"q3", '0'}, "q2"}
-         ,{{"q3", '1'}, "q2"}},
-	 "q1",
-	 { "q2" }
-    };
+    DFA machine{ states, alpha, func, start_state, accept_states };
+
+    CHECK(machine.getStates() == states);
+    CHECK(machine.getAlphabet() ==  alpha);
+    CHECK(machine.getTransFunc() ==  func);
+    CHECK(machine.getStartState() ==  start_state);
+    CHECK(machine.getAcceptStates() == accept_states);
 }
